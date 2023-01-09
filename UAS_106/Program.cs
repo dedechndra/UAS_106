1a4d2a5cff1f8134f1ffeed9040bff07d533b28a
Rifa <rifafahim21.com@gmial.com>
2023-01-09 16:33:40 +0700

    Add project files.

---
+++ UAS_106/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_106
+{
+    class Program
+    {
+        /*A node class consi
+         * 
+         */
+        class node
+        {
+            public string info;
+            public node lchild;
+            public node rchild;
+            //constructor for the node class
+
+            public node(string i, node l, node r)
+            {
+                info = i;
+                lchild = l;
+                rchild = r;
+            }
+        }
+    }
+}
+
+
+        
